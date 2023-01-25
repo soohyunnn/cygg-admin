bbca977b0721c955f2e238b4fe920a21db53f46c
soohyunnn <sh.choi@cbcm-co.com>
2023-01-25 12:14:07 +0900

    first commit

---
+++ src/main/java/com/adm/cygg/CyggAdminApplication.java
@@ -0,0 +1,13 @@
+package com.adm.cygg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyggAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyggAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adm/cygg/CyggAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adm.cygg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyggAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
